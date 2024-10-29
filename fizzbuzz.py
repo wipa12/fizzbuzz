edb60b83f57b9f3d3dd72f810134dc727533af3d
wipa12 <wipa12@gmail.com>
2024-10-29 17:39:20 -0400

    fizzbuzzdemo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
-
+number = 1
+while number <= 100:
+    if number % 3 == 0 and number % 5 == 0:
+       print("FizzBuzz")
+    elif number % 3 == 0:
+           print("fizz")
+    elif number % 5 == 0:
+       print("buzz")   
+    else: 
+     print(number)
+    number = number + 1
