634bf4fc51c570661863f2e38ec807b81c09b484
wipa12 <wipa12@gmail.com>
2024-11-01 19:02:35 -0400

    added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,8 @@
+## set vaiable number to value
 number = 1
+## cereate while loop comparing to set number
+## check number to see if divisible by given number
+## output desired word
 while number <= 100:
     if number % 3 == 0 and number % 5 == 0:
        print("FizzBuzz")
@@ -8,4 +12,5 @@ while number <= 100:
        print("Buzz")
     else:
        print(number)
+## update variable by 1 ubtil loop is finished       
     number = number + 1
