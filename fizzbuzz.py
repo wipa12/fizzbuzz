7ee97c181b06141ba4579099665f0a14b8058b4b
wipa12 <wipa12@gmail.com>
2024-11-01 18:52:46 -0400

    modified fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,9 @@ while number <= 100:
     if number % 3 == 0 and number % 5 == 0:
        print("FizzBuzz")
     elif number % 3 == 0:
-           print("fizz")
+       print("Fizz")
     elif number % 5 == 0:
-       print("buzz")   
-    else: 
-     print(number)
+       print("Buzz")
+    else:
+       print(number)
     number = number + 1
